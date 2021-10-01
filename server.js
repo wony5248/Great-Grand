521f6adcafe5d3dfa3235efff5f5b90ab2b4d11d
unknown <dlwlgns102@naver.com>
2021-10-01 16:01:06 +0900

    feat: socket server

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ const fs = require("fs");
 const { Server } = require("http");
 require('dotenv').config();
 
+//----------------------------------
+// websocket
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port2 = 3002;
+
 // --------------------------------------------
 // env
 const envJson = require(`${__dirname}/env/env.json`);
@@ -51,24 +57,76 @@ app.use(session({
 //----------------------------------
 // routes
 app.use(uploadFilePath, express.static(path.join(__dirname + uploadFilePath)));
-app.use("/callback", require(`${__dirname}/routes/callback/callback`));
-app.use("/user", require(`${__dirname}/routes/user/user`));
-app.use("/fridge", require(`${__dirname}/routes/fridge/fridge`));
-app.use("/calendar", require(`${__dirname}/routes/calendar/calendar`));
-app.use("/foodlist", require(`${__dirname}/routes/calendar/foodlist`));
-app.use("/recipe", require(`${__dirname}/routes/recipe/recipe`));
+// app.use("/callback", require(`${__dirname}/routes/callback/callback`));
+// app.use("/user", require(`${__dirname}/routes/user/user`));
+// app.use("/fridge", require(`${__dirname}/routes/fridge/fridge`));
+// app.use("/calendar", require(`${__dirname}/routes/calendar/calendar`));
+// app.use("/foodlist", require(`${__dirname}/routes/calendar/foodlist`));
+// app.use("/recipe", require(`${__dirname}/routes/recipe/recipe`));
 
 
 app.get("/", function (req, res) {
   res.send("Hello CI/CD");
 });
 
-app.get("/img", function(req, res){
-  const rID = req.query.id;
-  fs.readFile(`../../images/${rID}`, function(err, data){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
-  })
+// app.get("/img", function(req, res){
+//   const rID = req.query.id;
+//   fs.readFile(`../../images/${rID}`, function(err, data){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end(data);
+//   })
+// })
+
+const roomName = 'team';
+
+io.on('connection', function(socket) {
+  socket.join(roomName);
+
+  // 로직 3. 사용자의 메시지 수신시 WebClient로 메시지 전달
+  socket.on('safety_status', (message) => {
+    socket.to(roomName).emit('sendSafetyStatus', message);
+  });
+
+  socket.on('PatrolStatus', (message) => {
+    socket.to(roomName).emit('sendPatrolStatus', message);
+  });
+
+  socket.on('PatrolOnToServer', (data) => {
+    socket.to(roomName).emit('patrolOn', data);
+    console.log('Patrol On!');
+  });
+
+  socket.on('PatrolOffToServer', (data) => {
+    socket.to(roomName).emit('patrolOff', data);
+  });
+
+  socket.on('turnleftToServer', (data) => {
+    socket.to(roomName).emit('turnleft', data);
+  });
+
+  socket.on('gostraightToServer', (data) => {
+    socket.to(roomName).emit('gostraight', data);
+  });
+
+  socket.on('turnrightToServer', (data) => {
+    socket.to(roomName).emit('turnright', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnected from server');
+  });
+
+  // 전달받은 이미지를 jpg 파일로 저장
+  socket.on('streaming', (message) => {
+    socket.to(roomName).emit('sendStreaming', message);
+    // console.log(message);
+    buffer = Buffer.from(message, "base64");
+    fs.writeFileSync(path.join(picPath, "/../client/cam.jpg"), buffer);
+  });
+})
+
+server.listen(port2, ()=>{
+  console.log(`{init : ${port2}}`);
 })
 
 app.listen(port, () => {
